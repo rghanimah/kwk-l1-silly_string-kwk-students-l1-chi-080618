05ff81d430fe54f0f1b097b8d82f7bfc77138ae3
rghanimah <rabiaghanimah@gmail.com>
2018-08-06 21:32:59 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -19,9 +19,11 @@ puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
 phrase = "IM NOT SHOUTING".downcase
 
-# lowercased_phrase = phrase # Put a . after phrase and use a String method.
+lowercased_phrase = phrase 
 
-# puts lowercased_phrase
+# Put a . after phrase and use a String method.
+
+puts lowercased_phrase
 
 # Challenge 4: Count how many letters are in that big_word
 
