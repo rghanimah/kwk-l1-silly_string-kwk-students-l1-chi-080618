7ae1da76312dd72cacab2434881931878d23c6c0
rghanimah <rabiaghanimah@gmail.com>
2018-08-06 21:35:49 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -41,7 +41,7 @@ puts "There are #{letter_count} in #{big_word}"
 # bigger_word = big_word 
 
 # Use String concatenation to add "wow" and "mom" to big_word
-bigger_word = big_word + "mom" + "wow"
+bigger_word = big_word + "wow" + "mom"
 new_letter_count = bigger_word.size
 
 puts "There are now #{new_letter_count} in #{bigger_word}"
