85544f9c941e8e5791bcfd35a5b86221f09722a7
rghanimah <rabiaghanimah@gmail.com>
2018-08-06 21:33:39 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -27,9 +27,9 @@ puts lowercased_phrase
 
 # Challenge 4: Count how many letters are in that big_word
 
-big_word = "supercalifragilisticexpialidocious".length
+big_word = "supercalifragilisticexpialidocious"
 letter_count = big_word.length
-letter_count = big_word 
+# letter_count = big_word 
 
 
 # Put a . after big_word and use a String method.
